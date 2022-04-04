38fd26d9def068441b39093aad16f7ee1e075f74
KuraiSora16 <61778437+KuraiSora16@users.noreply.github.com>
2022-04-04 22:57:24 +0800

    add a somewhat working statistics scripts

---
+++ Assets/Scripts/ButtonHandlers/StudentStatisticsBH.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class StudentStatisticsBH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button goBackButton, exitButton, searchButton;
+
+    public InputField studentName;
+
+    [SerializeField]
+    private GameObject statisticsTemplate;
+
+    public List<GameObject> statistics = new List<GameObject>();
+
+    public IEnumerator coroutine;
+
+    void Start()
+    {
+        goBackButton.onClick.AddListener(goBackMainMenu);
+        exitButton.onClick.AddListener(exitGame);
+        searchButton.onClick.AddListener(searchStudent);
+
+        // Set the assignment_id for the GameManager to retreive questions | Default is 0
+    }
+    void exitGame()
+    {
+        Debug.Log("Exit selected. Quit Application.");
+        Application.Quit();
+    }
+
+    void goBackMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu (Teacher)");
+    }
+
+    void searchStudent()
+    {
+        LoadLeaderboardFromDatabase(studentName.text);
+    }
+
+    void LoadLeaderboardFromDatabase(string username)
+    {
+        coroutine = get_leaderboard(username);
+        StartCoroutine(coroutine);
+    }
+
+    public IEnumerator get_leaderboard(string username1)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username1);
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/fetch_statistics.php", form)) // sending inputs to be queried, will be done by php
+        {
+            yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                //Debug.Log(www.downloadHandler.text); // The json string returned by the php file
+                string jsonArray = www.downloadHandler.text;
+                //Debug.Log(jsonArray);
+
+                leaderboard_list leaderboardList = JsonUtility.FromJson<leaderboard_list>("{\"leaderboard_data\": " + jsonArray + "}");
+
+                var position = statisticsTemplate.transform.position;
+                var y = position.y;
+                for (int i = 0; i < leaderboardList.leaderboard_data.Length; i++)
+                {
+                    GameObject rank = Instantiate(statisticsTemplate) as GameObject;
+                    rank.SetActive(true);
+
+                    string username = leaderboardList.leaderboard_data[i].username;
+                    string score = leaderboardList.leaderboard_data[i].score;
+                    string subject = leaderboardList.leaderboard_data[i].topic;
+                    string level = leaderboardList.leaderboard_data[i].difficulty;
+
+                    Text userText = rank.transform.GetChild(0).GetComponent<Text>();
+                    userText.text = username;
+                    Text scoreText = rank.transform.GetChild(1).GetComponent<Text>();
+                    scoreText.text = score;
+                    Text subjectText = rank.transform.GetChild(2).GetComponent<Text>();
+                    subjectText.text = subject;
+                    Text levelText = rank.transform.GetChild(3).GetComponent<Text>();
+                    levelText.text = level;
+
+                    rank.transform.SetParent(statisticsTemplate.transform.parent, false);
+                    rank.transform.position = new Vector3(position.x, y, position.z);
+                    statistics.Add(rank.gameObject);
+                    y = y - 250;
+                }
+            }
+        }
+    }
+}
